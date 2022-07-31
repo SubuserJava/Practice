04d136b2bfaf0a09ff1b6591f942d6d4dce95518
SubuserJava <telemarent@yandex.ru>
2022-08-01 01:58:03 +0300

    Edit 'Box' package.

--- src/com/company/practice/ObjectOrientedProgramming/Box/BoxDemo.java
+++ src/com/company/practice/ObjectOrientedProgramming/Box/BoxDemo.java
@@ -4,15 +4,15 @@ public class BoxDemo {
 
     public static void main(String[] args) {
 
-//        Box mybox = new Box();
-//
-//        myBox.width = 10;
-//        myBox.height = 20;
-//        myBox.depth = 15;
-//
-//        double vol = myBox.width *  myBox.height * myBox.depth;
-//
-//        System.out.println(vol);
-//
+        Box myBox = new Box();
+
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        double vol = myBox.width *  myBox.height * myBox.depth;
+
+        System.out.println(vol);
+
     }
 }
